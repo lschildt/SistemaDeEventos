3abd679edbbfdb5f5eb0fbab496b4574825ae1c7
Cassio Luis Pires <cle@trf4.jus.br>
2016-09-27 21:52:42 -0300

    Atualização faltante do método adicionarIngresso.

--- src/br/edu/SistemaDeEventos/servico/IngressoVIPServico.java
+++ src/br/edu/SistemaDeEventos/servico/IngressoVIPServico.java
@@ -1,5 +1,26 @@
 package br.edu.SistemaDeEventos.servico;
 
-public class IngressoVIPServico {
+import java.util.ArrayList;
+
+import br.edu.SistemaDeEventos.modelo.Evento;
+import br.edu.SistemaDeEventos.modelo.Ingresso;
+
+public class IngressoVIPServico implements IngressoServico {
+	
+	public Evento adicionarIngresso(Evento evento) {
+		ArrayList<Ingresso> Ingresso = evento.getIngressosDisponiveisEvento();
+		
+		boolean ingressoEstaNalista = false;
+		for (int i = 0; i < Ingresso.size(); i++) 
+		  if (Ingresso.get(i).getTipo() == "VIP")
+			ingressoEstaNalista = true;
+	
+		if (!ingressoEstaNalista) {
+  		  Ingresso.add(new Ingresso("VIP", 1000.00));
+  		  evento.setIngressosDisponiveisEvento(Ingresso);
+		}
+		
+		return evento;
+	}
 
 }
