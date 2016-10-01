ca95a6da98f0e98e2e62022bf65a699432f7c49c
LEANDRO SCHILDT <ALU201010170@uniritter.local>
2016-10-01 09:08:12 -0300

    Ajuste hamcrest.

---
+++ test/br/edu/SistemaDeEventos/servico/EventoServicoTest.java
@@ -0,0 +1,21 @@
+package br.edu.SistemaDeEventos.servico;
+
+import org.junit.Test;
+
+public class EventoServicoTest {
+
+	private EventoServico eventoServico;
+	
+	
+	
+	private void setUp() {
+		eventoServico = new EventoServico();
+	}
+	
+	@Test
+	public void calcularDescontoIngressosGold() {
+		
+	}
+	
+	
+}
